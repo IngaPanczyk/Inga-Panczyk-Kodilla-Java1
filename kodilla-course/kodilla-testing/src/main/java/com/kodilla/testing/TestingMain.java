5e0b23c40c928dcbef9217be746e4cbedaee34b0
IngaPanczyk <ingawaaa@gmail.com>
2020-06-09 21:21:43 +0200

    Calculator1

--- kodilla-course/kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-course/kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        //Test Calculator
+        Calculator calculator = new Calculator();
+        calculator.add(6,8);
+        System.out.println("The score is: " + calculator.getScore1());
+
+        Calculator calculator1 = new Calculator();
+        calculator.subtract(180,18000);
+        System.out.println("The score is: " + calculator1.getScore2());
+
     }
 }
